6b6be13f7a662f1f6f1eb8ccda5be5ff4bf66376
Deepak Kumar <deepakdbg150219@gmail.com>
2023-04-27 17:34:40 +0530

    pushing second commit

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models.product import Product
 from .models.category import Category
@@ -30,8 +30,42 @@ def signup(request):
         phone=postData.get('number')
         email=postData.get('email')
         password=postData.get('password')
-        print(first_name,last_name,phone,email,password)
-        customer=Customer(first_name=first_name,last_name=last_name,phone=phone,email=email,password=password)
-        customer.register()
+        #validation
 
-        return HttpResponse("Recieved post request")
+        value={
+            'first_name':first_name,
+            'last_name':last_name,
+            'phone':phone,
+            'email':email,
+        }
+        error_message=None
+
+        if not first_name:
+            error_message="First name is required"
+        elif len(first_name)<4:
+            error_message="First name must be at least 4 characters"
+        elif not last_name:
+            error_message="Last name is required"
+        elif len(last_name)<4:
+            error_message="Last name must be at least 4 characters"
+        elif not phone:
+            error_message="Phone number is required"
+        elif len(phone)<10:
+            error_message="Phone number must be at least 10 characters"
+        elif len(password)<6:
+            error_message="Password must be at least 6 characters"
+        elif len(email)<6:
+            error_message="Email must be at least 6 characters"
+
+        #saving
+        if not error_message:
+            print(first_name,last_name,phone,email,password)
+            customer=Customer(first_name=first_name,last_name=last_name,phone=phone,email=email,password=password)
+            customer.register()
+            return redirect('homepage')
+        else:
+            data={
+                'error':error_message,
+                'values':value
+            }
+            return render(request, 'signup.html',data)
