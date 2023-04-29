8b732649289cfba5df8a9d4dfbd5e7534ff62646
Deepak Kumar <deepak_kumar10@persistent.com>
2023-04-30 00:01:40 +0530

    checkout page is half done

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import home,login, signup
 from .views.login import logout
 from .views.cart import Cart
+from .views.checkout import CheckOut
+from .views.orders import OrderView
 
 urlpatterns = [
     path('',home.Index.as_view(),name='homepage'),
@@ -9,4 +11,6 @@ urlpatterns = [
     path('login',login.Login.as_view(),name='login'),
     path('logout',logout, name='logout'),
     path('cart',Cart.as_view(),name='cart'),
+    path('check-out',CheckOut.as_view(),name='checkout'),
+    path('orders',OrderView.as_view(),name='orders'),
 ]
---
+++ store/views/orders.py
@@ -0,0 +1,14 @@
+from django.views import View
+from ..models.customer import Customer
+from django.shortcuts import render,redirect
+from store.models.product import Product
+from store.models.orders import Order
+
+
+class OrderView(View):
+    def get(self,request):
+        customer=request.session.get('customer')
+        orders=Order.get_order_by_customer(customer)
+        print(orders)
+        return render(request,'orders.html',{'orders':orders})
+    
