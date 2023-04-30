91ac7f01be9b7fc639fe370b9a1cf908f225bb15
Deepak Kumar <deepak_kumar10@persistent.com>
2023-04-30 13:40:05 +0530

    completed project

--- store/views/orders.py
+++ store/views/orders.py
@@ -3,6 +3,7 @@ from ..models.customer import Customer
 from django.shortcuts import render,redirect
 from store.models.product import Product
 from store.models.orders import Order
+# from store.middleware.auth import auth_middleware
 
 
 class OrderView(View):
